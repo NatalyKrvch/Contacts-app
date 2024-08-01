f31282b9225fcbf82172eaa0d249f38d385bed31
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2024-08-01 20:29:38 +0300

    chore: rename globalConstants -> index

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
 export const API_URL = process.env.REACT_APP_API_URL
 export const CORS_PROXY_URL = process.env.REACT_APP_PROXY_URL
 export const AUTH_TOKEN = process.env.REACT_APP_AUTH_TOKEN
+
+export const EMPTY_LENGTH = 0
