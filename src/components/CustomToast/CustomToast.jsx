c9bd79fe20b903049b18ceef4b884f29e0281787
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2024-08-02 01:27:41 +0300

    feat: enhance CustomToast

--- src/components/CustomToast/CustomToast.jsx
+++ src/components/CustomToast/CustomToast.jsx
@@ -1,11 +1,11 @@
 import { Toast, ToastContainer } from 'react-bootstrap'
 
-const CustomToast = ({ show, message, onClose }) => {
+const CustomToast = ({ show, label, message, onClose, bg = 'danger' }) => {
   return (
     <ToastContainer position="top-end" className="p-3">
-      <Toast show={show} onClose={onClose} bg="danger" delay={3000} autohide>
+      <Toast show={show} onClose={onClose} bg={bg} delay={3000} autohide>
         <Toast.Header>
-          <strong className="me-auto">Error</strong>
+          <strong className="me-auto">{label}</strong>
         </Toast.Header>
         <Toast.Body>{message}</Toast.Body>
       </Toast>
