a08cd5ef7e4b4b1f8dfe40ec35da2cb712dc8bdd
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2024-08-02 23:07:23 +0300

    feat: improve styles

--- src/components/ContactForm/constants.js
+++ src/components/ContactForm/constants.js
@@ -1,13 +1,3 @@
-export const FORM_STYLES = {
-  position: 'sticky',
-  top: '0',
-  height: '100vh',
-  width: '280px',
-  backgroundColor: 'white',
-  padding: '1rem',
-  margin: '0',
-}
-
 export const FORM_FIRST_NAME_ID = 'formFirstName'
 export const FORM_LAST_NAME_ID = 'formLastName'
 export const FORM_EMAIL_ID = 'formEmail'
