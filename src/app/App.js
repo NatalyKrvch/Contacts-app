56532aba656fa35fc24f8b9537dbb71064dbdea0
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2024-08-04 22:30:17 +0300

    fix: deployment settings

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,20 @@
-import { Route, Routes } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import HomePage from '../pages/HomePage/HomePage'
-import ContactPage from '../pages/ContactPage/ContactPage'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import HomePage from '../pages/HomePage/HomePage';
+import ContactPage from '../pages/ContactPage/ContactPage';
+import './App.css';
 
 function App() {
   return (
-    <Container className="app-container">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/contact/:id" element={<ContactPage />} />
-      </Routes>
-    </Container>
-  )
+    <Router basename="/Contacts-app">
+      <Container className="app-container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact/:id" element={<ContactPage />} />
+        </Routes>
+      </Container>
+    </Router>
+  );
 }
 
-export default App
+export default App;
