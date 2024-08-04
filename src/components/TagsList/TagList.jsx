6225709b6a0fea308a6787115bf70d417bcbc45d
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2024-08-05 00:42:32 +0300

    fix: add data-testid to TagList

--- src/components/TagsList/TagList.jsx
+++ src/components/TagsList/TagList.jsx
@@ -4,7 +4,7 @@ const TagsList = ({ tags = [] }) => {
   return (
     <div>
       {tags.map(({ id, tag }) => (
-        <Badge key={id} bg="secondary" className="me-2 mb-3 fw-light">
+        <Badge key={id} bg="secondary" className="me-2 mb-3 fw-light" data-testid={`tag-${id}`}>
           {tag}
         </Badge>
       ))}
