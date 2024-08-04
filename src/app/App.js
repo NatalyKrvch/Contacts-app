1463b1d36c506f33efc985d63620f1246aa13fd9
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2024-08-04 22:56:47 +0300

    fix: routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,20 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import HomePage from '../pages/HomePage/HomePage';
-import ContactPage from '../pages/ContactPage/ContactPage';
-import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import HomePage from '../pages/HomePage/HomePage'
+import ContactPage from '../pages/ContactPage/ContactPage'
+import './App.css'
 
 function App() {
   return (
-    <Router basename="/Contacts-app">
-      <Container className="app-container">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/contact/:id" element={<ContactPage />} />
-        </Routes>
-      </Container>
-    </Router>
-  );
+    <Container className="app-container">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact/:id" element={<ContactPage />} />
+      </Routes>
+    </Container>
+  )
 }
 
-export default App;
+export default App
